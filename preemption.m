672c7561412edbfed36e8e83e76cad019520a842
Poindexter <kurt2718@users.noreply.github.com>
2017-09-01 23:59:40 +0200

    update function for a single source complete, now for the social network implementation

--- preemption.m
+++ preemption.m
@@ -2,9 +2,9 @@
 clear all;
 
 % PARAMETERS
-global NUM_AGENTS = 2;
+global NUM_AGENTS = 3;
 global NUM_TSTEPS = 49;
-global NUM_TRUST_RESOLUTION = 99;
+global NUM_TRUST_RESOLUTION = 199;
 
 % In my 'elaborate' model, the target proposition is true
 global p = true;
@@ -19,7 +19,7 @@ global t = 1;
 
 % Holds all values that the reliability can take 
 % Used as X-Vector for PDFs
-global rho = linspace(0,1,NUM_TRUST_RESOLUTION);
+global rho = linspace(0.0001,0.9999,NUM_TRUST_RESOLUTION);
 
 
 
@@ -40,21 +40,24 @@ end
 % Calculates and returns the Expectation (Mean, Expected Value)
 % of @pdf over [0,1] with resolution NUM_TRUST_RESOLUTION
 % by approximating the integral. Crank up resolution to get more precise.
-function expectation = calcExpectation(pdf)
+function e = expectation(pdf)
     global rho NUM_TRUST_RESOLUTION;
     % Check that @pdf is a row vector
     if (~isvector(pdf) || size(pdf) ~= [1,NUM_TRUST_RESOLUTION])
         error('PDF Argument not valid. Must be size 1xNUM_TRUST_RESOLUTION')
     end
     pdf = pdf .* rho;
-    expectation = trapz(rho,pdf);
+    e = trapz(rho,pdf);
+    % Sometimes the approx can be out of bounds, so better be save
+    if (e > 1) e = 1;end
+    if (e < 0) e = 0;end
 end
 
-function drawpdf(pdf, linespec)
+function plotpdf(pdf, color)
     global rho NUM_TRUST_RESOLUTION;
-    plot(rho,pdf,linespec);
-    e = calcExpectation(pdf);
-    plot([e e], ylim, linespec);
+    plot(rho,pdf,'Color',color);
+    e = expectation(pdf)
+    plot([e e], ylim, color);
 end
 
 function setup()
@@ -65,51 +68,68 @@ function setup()
     % (agent, attribute, time)
     % Set the agents attributes: 
     % 1 = Activity, 2 = Aptitude, 3 = Credence (in p)
-    % Starting Credences are Randomized
+
+    % Starting Credences are uniformly distributed  
     agents = rand(NUM_AGENTS, 3, 1);
-    agents(:,1,1) = 1; 
-    agents(:,2,1) = 1;
-    agents(:,3,1) = 0.8;
+    
+    % For this simple model, there is just one Expert
+    % AGENT 1 = EXPERT
+    % AGENT 2 = TOTAL EVIDENCE STRATEGY
+    % AGENT 3 = PREEMPTION STRATEGY
+    agents(1,:,1) = [0.8 0.9 0.9];
+
+    agents(2:end,1,1) = 0.5; 
+    agents(2:end,2,1) = 0.5;
+    agents(2:end,3,1) = 0.5;
+    agents
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
     % SETUP TRUST FUNCTIONS
     % trusts(i,j,:): Trust function of agent i to agent j 
-
     trusts = zeros(NUM_AGENTS,NUM_AGENTS,NUM_TRUST_RESOLUTION);
-    trusts(1,1,:) = betapdf(rho,5,4.5);
-    trusts(1,2,:) = betapdf(rho,5,3);
-    trusts(2,1,:) = betapdf(rho,2,5);
-    trusts(2,2,:) = betapdf(rho,5,2);
+    % The Expert knows herself to be an expert
+    trusts(1,1,:) = betapdf(rho,5,0.4);
 
-    %hold on
+    for i = 2:NUM_AGENTS 
+        % The Agents trust the expert already pretty much
+        trusts(i,1,:) = betapdf(rho,5,1.2);
+        % The Agents are slightly overconfident
+        trusts(i,i,:) = betapdf(rho,5,4);
+        % The expert doesnt think the other agents to be very helpful
+        trusts(1,i,:) = betapdf(rho,5,5);
+    end
 
-    %tr =squeeze(trusts(1,1,:));
-    %plot(rho,tr,'-r');
-    %e = calcExpectation(squeeze(tr)')
-    %plot([e e], ylim, '--r');
-
-    %tr =trusts(1,2,:);
-    %plot(rho,tr,'-g');
-    %e = calcExpectation(squeeze(tr)')
-    %plot([e e], ylim, '--g');
-
-
-    %tr =trusts(2,1,:);
-    %plot(rho,tr,'-b');
-    %e = calcExpectation(squeeze(tr)')
-    %plot([e e], ylim, '--b');
-
-
-    %tr =trusts(2,2,:);
-    %plot(rho,tr,'-k');
-    %e = calcExpectation(squeeze(tr)')
-    %plot([e e], ylim, '--k');
-
-    %hold off
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+    % SETUP NETWORK
+    % network(i,j): There is a link from agent i to agent j
+    % i 'listens to' j
+    network = zeros(NUM_AGENTS, NUM_AGENTS);
+    network(2,1) = 1;
+    network(3,1) = 1;
 end
 
 % One Complete timestep
+% Every Timestep, the following happens:
+% 1) with a chance, the expert inquires
+%   If inquired
+%   1.1) Updates Credence
+%   1.2) Updates Trust Function
+%   1.3) if inquired, and if sufficiently confident, presents new findings to agents
+% (This can be stupid, since if the expert is less condident than before but still confident enough, the agent will get more confident which is the opposite of the intended effect. Needs examination).
+% 2) With a chance, agents inquire themselves.
+% 3) Total Evidence Agents combine their sources to update their opinion
+% 4) Preemption Agents will adopt experts opinion if they regard them as an authority, otherwise like TEA.
+% 5) Trust functions get updated
 function step()
     global rho t agents trusts NUM_AGENTS;
+
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+    % 1) Expert Inquiry
+    if (coin(agents(1,1,1)))
+
+    end
 
     for i = 1:NUM_AGENTS
         % Activity and Aptitude are constant over time 
@@ -122,8 +142,9 @@ function step()
             s = coin(aptitude);
 
             % Calculate new Credence from Prior, Trust, and their inverses
+            % cf. Angere(ms), p.4/5
             c = agents(i,3,t);
-            tau = calcExpectation(squeeze(trusts(i,i,:))'); 
+            tau = expectation(squeeze(trusts(i,i,:))'); 
             nc = 1 - c ;
             ntau = 1 - tau;
             if (s == 1)
@@ -133,19 +154,20 @@ function step()
             end
 
             % Calculate new Trust function
+            % cf. Angere(ms), p.8/9
             c = agents(i,3,t);
-            tau = calcExpectation(squeeze(trusts(i,i,:))');
+            exptau = expectation(squeeze(trusts(i,i,:))');
             nc = 1 - c;
-            ntau = 1 - tau;
+            nexptau = 1 - exptau;
 
-            tauDummy = squeeze(trusts(i,i,:))';
-            %if (s == 1)
-            nom = (rho*c) .+ ((1-rho)*nc)
-            dnom = tau*c + ntau*nc
-            foo =  nom / dnom
-            newtau = tauDummy .* foo;
-            trusts(i,i,:) = newtau;
-            %end
+            tau = squeeze(trusts(i,i,:))';
+            if (s == 1)
+                dnom = exptau*c + nexptau*nc;
+                trusts(i,i,:) = tau .* (((rho*c) .+ ((1-rho)*nc)) / dnom);
+            else
+                dnom = exptau * nc + nexptau * c;
+                trusts(i,i,:) = tau .* (((rho*nc) .+ ((1-rho)*c)) / dnom);
+            end
         end
     end
 
@@ -155,30 +177,13 @@ end
 function main()
     global NUM_TSTEPS NUM_AGENTS agents trusts;
     setup();
-    hold on
-    drawpdf(squeeze(trusts(1,1,:))','-r');
-    step();
-    drawpdf(squeeze(trusts(1,1,:))','-g');
-    %for i = 1:NUM_TSTEPS
-    %
-    %step();
+    %for i = 1:99
+        %step();
+        %hold on
+    %plotpdf(squeeze(trusts(1,1,:))', [i/100,0,0]);
+
     %end
-    %hold on
-    %for i = 1:NUM_AGENTS
-    %plot(squeeze(agents(i,3,:)));
-    %end
-    %hold off
 end
 
 main();
 
-%hold on
-%for i = 1:NUM_AGENTS
-%credences = squeeze(agents(i,3,:))
-%length(credences)
-%length(0:NUM_TSTEPS)
-%length(1:NUM_TSTEPS)
-%plot(0:NUM_TSTEPS,credences);
-%end
-%pause
-%hold off
