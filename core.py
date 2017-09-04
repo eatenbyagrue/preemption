42df8f4e97e3a30e64b79e2393b88f7c58cac114
Poindexter <kurt2718@users.noreply.github.com>
2017-09-04 17:17:49 +0200

    run a linter. Now everything up to style

--- core.py
+++ core.py
@@ -4,36 +4,40 @@ import scipy.stats as stats
 import scipy.integrate as integ
 import matplotlib.pyplot as plt
 
-################################################################################
+###############################################################################
 # Parameters
 # Model assumes target propostion P to be true
 NUM_AGENTS = 3
 TIME_STEPS = 49
-TRUST_RESOLUTION = 999
+TRUST_RESOLUTION = 99
 ASSERT_THRESHOLD = 0.7999
 
-################################################################################
+###############################################################################
 # Initialisations
-rho = np.linspace(0.0001,0.9999,TRUST_RESOLUTION)
+rho = np.linspace(0.0001, 0.9999, TRUST_RESOLUTION)
 Agents = 0
 Trusts = 0
 t = 0
 
-# Flips a true/false-coin with @bias to true
+
 def fcoin(bias):
-    return np.random.binomial(1,bias)
+    """Flips a true/false-coin with @bias to true
+    """
+    return np.random.binomial(1, bias)
+
 
 def print3(T):
     # Prints 3dim Array T like this:
     # for each z, T[i,j,z] is a matrix with i rows and j columns
-    print("Array with dimensions",str(np.shape(T)))
-    for k in range(0,np.shape(T)[2]):
-        print(T[:,:,k])
+    print("Array with dimensions", str(np.shape(T)))
+    for k in range(0, np.shape(T)[2]):
+        print(T[:, :, k])
+
 
 def setup():
     global Agents, Trusts
 
-    ############################################################################
+    ###########################################################################
     # SETUP AGENTS
     # Agents(i,j,t). i = agent, j = attribute, t = time
     # j = 0: activity, j = 1: aptitude, j = 2: credence
@@ -44,27 +48,26 @@ def setup():
     # AGENT 1 = TOTAL EVIDENCE STRATEGY
     # AGENT 2 = PREEMPTION STRATEGY
     # The Expert is an expert
-    Agents[0,:,0] = [1, 0.2, 0.8]
+    Agents[0, :, 0] = [1, 0.9, 0.8]
     # The other agents aren't particularly good inquirer
-    Agents[1:,0,0] = 0.5
-    Agents[1:,1,0] = 0.5
-    Agents[1:,2,0] = 0.5
+    Agents[1:, 0, 0] = 0.9
+    Agents[1:, 1, 0] = 0.55
+    Agents[1:, 2, 0] = 0.55
 
-    ############################################################################
+    ###########################################################################
     # SETUP TRUST FUNCTIONS
 
     Trusts = np.zeros((NUM_AGENTS, NUM_AGENTS, TRUST_RESOLUTION))
-    Trusts[0,0,:] = stats.beta.pdf(rho,5,1.6)
-    e = expectation(Trusts[0,0,:])
+    Trusts[0, 0, :] = stats.beta.pdf(rho, 5, 1.6)
 
-    integral = integ.trapz(Trusts[1,1,:],dx = 1/TRUST_RESOLUTION)
-    for i in range(1,NUM_AGENTS):
+    for i in range(1, NUM_AGENTS):
         # The agents recognized the expert
-        Trusts[i,0,:] = stats.beta.pdf(rho,5,1.2)
+        Trusts[i, 0, :] = stats.beta.pdf(rho, 5, 1.2)
         # The agents are slightly overconfident
-        Trusts[i,i,:] = stats.beta.pdf(rho,5,4)
+        Trusts[i, i, :] = stats.beta.pdf(rho, 5, 4)
         # The expert does not think very much of the other agents
-        Trusts[0,i,:] = stats.beta.pdf(rho,5,5)
+        Trusts[0, i, :] = stats.beta.pdf(rho, 5, 5)
+
 
 def expectation(pdf):
     """Calculates the expected Value of the given pdf.
@@ -72,25 +75,24 @@ def expectation(pdf):
     Integration over x*pdf(x) with trapezoidal method.
     """
 
-    e = integ.trapz(pdf*rho,rho)
+    e = integ.trapz(pdf*rho, rho)
 
     # Correct for inaccuracies that result in out of bounds
-    if (e > 1): e = 1
-    if (e < 0): e = 0
+    if (e > 1):
+        e = 1
+    if (e < 0):
+        e = 0
 
-    print(e)
-    print('Integral', integ.trapz(pdf,rho))
     return e
 
-def update_credence(c, msgs, exps):
+
+def update_credence(c, ss):
     """Calculates the updated credence for an agent given new source messages
 
-    Takes old credence @cre, all new messages @msgs and the trust functions of
-    that source. Calculation according to Angere(ms) p.21 (NOT p.22).
+    Takes old credence @cre, all new communications @ss. that source.
+    Calculation according to Angere(ms) p.21 (NOT p.22).
 
     """
-
-
     # e = exps[0]
     # msg = msgs[0]
     # nc = 1-c
@@ -102,26 +104,31 @@ def update_credence(c, msgs, exps):
         # new_c = c*ne / (c*ne + nc*e)
 
     # return new_c
-    pterm = c
-    for i in range(0,len(msgs)):
-        pterm *= exps[i] if msgs[i] else 1 - exps[i]
 
+    pterm = c
     npterm = 1 - c
-    for i in range(0,len(msgs)):
-        npterm *= 1 - exps[i] if msgs[i] else exps[i]
+    for s in ss:
+        msg = s['msg']
+        e = s['e']
+        pterm *= e if msg else 1-e
+        npterm *= 1-e if msg else e
 
     new_c = pterm / (pterm + npterm)
 
     return new_c
 
-def update_trf(c,msg,trf,e):
+
+def update_trf(c, s):
     """Calculates the updated trust function given a new source message
 
     """
-    global rho
 
+    global Trusts, rho
+    msg = s['msg']
+    e = s['e']
     nc = 1 - c
     ne = 1 - e
+    trf = Trusts[s['to'], s['from'], :]
     new_trf = []
 
     if msg:
@@ -134,8 +141,9 @@ def update_trf(c,msg,trf,e):
 
     return new_trf
 
+
 def step(t):
-    """This is what happens every time step.
+    """This function describes a time step.
 
     1) With a Chance, the expert inquires. If inquired,
         1.1) Expert updates credence
@@ -147,25 +155,49 @@ def step(t):
         3) Updates trust functions
     3) Voila!
     """
-    print('########################### NEW STEP')
     global Agents, Trusts
 
     # Not very performant, better to initalize Agents with the right dimensions
-    Agents = np.dstack((Agents,Agents[:,:,t]))
+    Agents = np.dstack((Agents, Agents[:, :, t]))
 
     # Keeping score of all the communications for this timestep
-    # Structure: [msg, FROM, TO]
     S = []
 
     # Do ALL of the Inquiries first
     for i in range(NUM_AGENTS):
-        act = Agents[i,0,0]
-        apt = Agents[i,0,0]
+        act = Agents[i, 0, 0]
+        apt = Agents[i, 1, 0]
 
         if fcoin(act):
-            inq = fcoin(apt)
-            S.append([inq,i,i])
+            S.append({'msg': fcoin(apt),
+                      'e': expectation(Trusts[i, i, :]),
+                      'from': i,
+                      'to': i
+                      })
+            # Add here the Experts communication to the other agentsq
+            c = Agents[0, 2, t+1]
+            if c > ASSERT_THRESHOLD or c < 1 - ASSERT_THRESHOLD:
+                for i in range(1, NUM_AGENTS):
+                    S.append({'msg': round(c),
+                              'e': expectation(Trusts[i, 0, :]), 
+                              'from': 0,
+                              'to': i 
+                              })
 
+    # Do ALL of the updating now
+    for i in range(NUM_AGENTS):
+        # Get all communications for this particular agent
+        # Not very performant, I recon
+        ss = [s for s in S if s['to'] == i]
+
+        # [print(trf) for trf in trfs]
+        c = Agents[i, 2, t]
+
+        if len(ss) > 0:
+            Agents[i, 2, t+1] = update_credence(c, ss)
+
+        for s in ss:
+            Trusts[s['to'], s['from'], :] = update_trf(c, s)
         # inq = fcoin(eapt)
         # print(inq)
         # # The expected value of the expert's trust function
@@ -188,19 +220,23 @@ def step(t):
         # Agents[i,2,t+1] = update_credence(c,msgs,
 
 
-
 setup()
 
-for i in range(0,50):
-    plt.subplot(2,1,1)
-    plt.plot(rho,Trusts[0,0,:])
-    if i%1 == 0:
-        plt.annotate(str(i),
-             xy=(rho[len(rho)-1], Trusts[0,0,len(rho) - 1]),
-             xycoords='data',
-             textcoords='offset points',)
-    plt.subplot(2,1,2)
-    plt.plot(Agents[0,2,:])
+for i in range(0, 50):
+    color = str(1 - (i/50))
+    plt.subplot(3, 2, 1)
+    plt.plot(rho, Trusts[0, 0, :], color=color)
+    plt.subplot(3, 2, 3)
+    plt.plot(rho, Trusts[1, 1, :], color=color)
+    plt.subplot(3, 2, 5)
+    plt.plot(rho, Trusts[2, 2, :], color=color)
+
+    plt.subplot(3, 2, 2)
+    plt.plot(Agents[0, 2, :])
+    plt.subplot(3, 2, 4)
+    plt.plot(Agents[1, 2, :])
+    plt.subplot(3, 2, 6)
+    plt.plot(Agents[2, 2, :])
     step(i)
 
 plt.show()
